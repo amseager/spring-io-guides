60260db7a281690c0b75bb6d3ec60f80d050a5f3
amseager <amseager@mail.ru>
2018-02-05 01:01:18 +0300

    gs-accessing-data-jpa | update test

--- gs-relational-data-access/src/test/java/hello/JdbcTemplateTest.java
+++ gs-relational-data-access/src/test/java/hello/JdbcTemplateTest.java
@@ -1,35 +1,33 @@
 package hello;
 
 import hello.domain.Customer;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import static org.assertj.core.api.Assertions.*;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
 @AutoConfigureTestDatabase
+@Transactional
 public class JdbcTemplateTest {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-    @Test
-    public void JdbcTemplateInsertTest() {
-
-        LOGGER.info("Creating tables...");
-
+    @Before
+    public void setUp() {
         jdbcTemplate.execute("DROP TABLE customers IF EXISTS");
         jdbcTemplate.execute("CREATE TABLE customers(" +
                 "id SERIAL, first_name VARCHAR(255), last_name VARCHAR(255))");
@@ -38,16 +36,17 @@ public class JdbcTemplateTest {
                 .map(name -> name.split(" "))
                 .collect(Collectors.toList());
 
-        splitUpNames.forEach(name -> LOGGER.info(String.format("Inserting customer record for %s %s", name[0], name[1])));
-
         jdbcTemplate.batchUpdate("INSERT INTO customers(first_name, last_name) VALUES (?,?)", splitUpNames);
+    }
 
-        LOGGER.info("Querying for customer records where first_name = 'Josh':");
-        jdbcTemplate.query(
+    @Test
+    public void jdbcTemplateQuery_WithFirstNameParam_ReturnsCustomersWithSpecificFirstName() {
+        List<Customer> customers = jdbcTemplate.query(
                 "SELECT id, first_name, last_name FROM customers WHERE first_name = ?"
                 , new Object[]{"Josh"}
                 , (rs, rowNum) -> new Customer(rs.getLong("id"), rs.getString("first_name"), rs.getString("last_name"))
-        ).forEach(customer -> LOGGER.info(customer.toString()));
-    }
+        );
 
+        assertThat(customers.size()).isEqualTo(2);
+    }
 }
